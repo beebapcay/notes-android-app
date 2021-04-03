53d8eeab593c4e6dd1fb3336365dce20de64105b
Phát Dương <69242061+beebapcay@users.noreply.github.com>
2021-04-03 16:14:10 +0700

    Create: Icon Launcher

--- NotesApp/app/src/main/java/com/beebapcay/notesapp/views/MainActivity.java
+++ NotesApp/app/src/main/java/com/beebapcay/notesapp/views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.beebapcay.notesapp;
+package com.beebapcay.notesapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.beebapcay.notesapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
